83763af074f6ba6b559203a7b1c01067ca192ef0
alexreusc <alexreusc@gmail.com>
2023-03-26 17:15:52 -0600

    move types to different file and solve bugs

---
+++ src/views/home/utils/types.ts
@@ -0,0 +1,36 @@
+type Category = "work" | "sports" | "health" | "culture" | "finance";
+type CompletionType = "normal" | "repetition" | "time";
+type DurationType = "day" | "month" | "year";
+
+interface GoalPortion {
+  initialDate: string;
+  finalDate: string;
+  completionState: number;
+}
+
+interface Goal {
+  id: string;
+  name: string;
+  portions: number;
+  completed: boolean;
+  category: Category;
+  completionType: CompletionType;
+  durationType: DurationType;
+  repetionsCompleted: number | null;
+  timeCompleted: number | null;
+  initialDate: string; //change to some type of date
+  finalDate: string;
+  timesToDo: number;
+  itGoalPortion: number;
+  goalPortions: GoalPortion[];
+}
+
+interface FieldError {
+  field: string;
+  message: string;
+}
+
+interface GoalCreated {
+  goal: Goal | null;
+  errors: FieldError[] | null;
+}
